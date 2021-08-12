491eaee50b667c27bb8bca19413b57cf5fc523d1
Samuel Mace <samuelcmace@gmail.com>
2021-08-11 19:12:53 -0500

    Simplify minecraft/process.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 const minecraft_module = require("./minecraft/process.js");
 
-var minecraft_instance = new minecraft_module("BEDROCK", path.join(__dirname, "server/bedrock_test_server"));
+// Create Minecraft Server Instances
+var bedrock_server = new minecraft_module("BEDROCK", path.join(__dirname, "server/bedrock_test_server"));
+var java_server = new minecraft_module("JAVA", path.join(__dirname, "server/java_test_server"));
 
+// Initalize Web Server
 const web_app = express();
 const web_port = 8080;
 
@@ -12,23 +15,28 @@ web_app.use(express.urlencoded({ extended:true }));
 web_app.use(express.json());
 
 // Set Redirects
-web_app.get("/", function(req, res) { res.redirect("/console.html") });
-web_app.get("/console", function(req, res) { res.redirect("/console.html") });
-web_app.get("/login", function(req, res) { res.redirect("/login.html") });
+web_app.get("/", function(req, res) { res.redirect("/console") });
+web_app.get("/console.html", function(req, res) { res.redirect("/console") });
+web_app.get("/login.html", function(req, res) { res.redirect("/login") });
 
 // Set Static Content
 web_app.use("/include/", express.static(path.join(__dirname, "src/", "include/")));
 web_app.use("/css/", express.static(path.join(__dirname, "src/", "css/")));
 
 // Set GET Requests
-web_app.get("/console.html", function(req, res) { res.sendFile(path.join(__dirname, "src/", "console.html")); });
-web_app.get("/login.html", function(req, res) { res.sendFile(path.join(__dirname, "src/", "login.html")); });
+web_app.get("/console", function(req, res) { res.sendFile(path.join(__dirname, "src/", "console.html")); });
+web_app.get("/login", function(req, res) { res.sendFile(path.join(__dirname, "src/", "login.html")); });
 
 // Set POST Requests
-web_app.post("/console.html", function(req, res) {
-  var command = String(req.body.text);
-  console.log("Web Interface Posted This Command: " + command);
-  minecraft_instance.runCommand(command);
+web_app.post("/console", function(req, res) {
+  var java_command = String(req.body.java_command);
+  var bedrock_command = String(req.body.bedrock_command);
+
+  console.log("Bedrock Web Interface Posted This Command: " + bedrock_command);
+  console.log("Java Web Interface Posted This Command: " + java_command);
+
+  bedrock_server.runCommand(bedrock_command);
+  java_server.runCommand(java_command);
 });
 
 web_app.post("/login.html", function(req, res) {
--- minecraft/process.js
+++ minecraft/process.js
@@ -2,8 +2,23 @@ const child_process = require("child_process");
 const path = require("path");
 
 module.exports = class MinecraftProcess {
-  java_run() {
-    this.process = child_process.spawn("java -jar server.jar",
+  constructor(type, server_directory) {
+    this.server_directory = server_directory;
+    this.server_type = type.toUpperCase();
+
+    switch(this.server_type) {
+      case "JAVA":
+        this.command = "java -Xmx1024M -Xms1024M -jar server.jar nogui";
+        break;
+      case "BEDROCK":
+        this.command = "LD_LIBRARY_PATH=. ./bedrock_server";
+        break;
+      default:
+        console.error('Minecraft Server Failed to Start: Invalid Server Type: Expected "JAVA" or "BEDROCK" (Received: ' + this.server_type + ')');
+        break;
+    }
+
+    this.server_process = child_process.exec(this.command,
       {cwd: this.server_directory},
       function(error, stdout, stderr) {
         if(error)
@@ -16,53 +31,13 @@ module.exports = class MinecraftProcess {
         console.log('Child Process STDERR: ' + stderr);
       }
     );
-
-    this.process.on('SIGTERM', function () {
-      console.log("Stopping the server...");
-      this.process.stdin.write("stop\n");
-      console.log("Success!");
-    });
-  }
-  bedrock_run() {
-    this.process = child_process.spawn("./bedrock_server",
-      {cwd: this.server_directory},
-      {env: "LD_LIBRARY_PATH=" + __dirname},
-      function(error, stdout, stderr) {
-        if(error)
-        {
-          console.log(error.stack);
-          console.log('Error code: ' + error.code);
-          console.log('Signal received: ' + error.signal);
-        }
-        console.log('Child Process STDOUT: ' + stdout);
-        console.log('Child Process STDERR: ' + stderr);
-      }
-    );
-
-    this.process.on('SIGTERM', function () {
-      console.log("Stopping the server...");
-      this.process.stdin.write("stop\n");
-      console.log("Success!");
+    this.server_process.on('exit', function () {
+      this.server_process.stdin.write("stop\n");
+      this.server_process.stdin.end();
     });
   }
   runCommand(command) {
-    this.process.stdin.write(command + "\n");
-  }
-  constructor(type, server_directory) {
-    this.process = undefined;
-    this.server_directory = server_directory;
-    this.serverType = type.toUpperCase();
-
-    if(this.serverType === "JAVA")
-    {
-      this.java_run();
-    }
-    else if(this.serverType === "BEDROCK")
-    {
-      this.bedrock_run();
-    }
-    else {
-      console.error('Error: Invalid Minecraft Server Type: Expected "JAVA" or "BEDROCK"');
-    }
+    this.server_process.stdin.write(command + "\n");
+    this.server_process.stdin.end();
   }
 }
